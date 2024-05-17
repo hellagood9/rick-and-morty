fc9a6ae93a2d2f6a449e671b4ce14abbb8144359
Maximiliano Sussini <maxsus60@gmail.com>
2024-05-17 20:41:18 +0200

    global styles

--- App.tsx
+++ App.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import {globalStyles} from './src/styles/global';
 
 function App(): React.JSX.Element {
   return (
-    <View style={styles.screenContainer}>
-      <View style={styles.content}>
-        <Text style={styles.sectionTitle}>Rick and Morty</Text>
+    <View style={globalStyles.screenContainer}>
+      <View style={globalStyles.content}>
+        <Text style={globalStyles.sectionTitle}>Rick and Morty</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    paddingHorizontal: 32,
-  },
-  content: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  sectionTitle: {
-    fontSize: 32,
-    fontWeight: '600',
-  },
-});
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    paddingHorizontal: 32,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sectionTitle: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+});
